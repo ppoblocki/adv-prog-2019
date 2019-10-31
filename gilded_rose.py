e2bce5ed849af150f10f0438176708cb6dd50d76
Patryk <patryk.drv@gmail.com>
2019-10-31 11:00:24 +0100

    Refactoring

--- gilded_rose.py
+++ gilded_rose.py
@@ -11,35 +11,33 @@ class GildedRose(object):
         for item in self.items:
             if not itemIsNamed(item, "Aged Brie") and not itemIsNamed(item,"Backstage passes to a TAFKAL80ETC concert"):
                 if itemHasNonZeroValue(item):
-                    if itemIsNamed(item, "Conjured Mana Cake"):
-                        item.quality = item.quality - 2
-                    elif not itemIsNamed(item, "Sulfuras, Hand of Ragnaros"):
-                        item.quality = item.quality - 1
+                    if item.isItemConjured():
+                        item.decreaseQualityBy(2)
+                    elif not item.isItemSulfuras():
+                        item.decreaseQualityBy(1)
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
+                if isUnderHighestQualityValue(item):
+                    item.incrementQuality()
                     if itemIsNamed(item, "Backstage passes to a TAFKAL80ETC concert"):
-                        if item.sell_in < 6 and item.quality < 50:
-                            item.quality += 1
                         if item.sell_in < 11:
                             if isUnderHighestQualityValue(item):
-                                item.quality = item.quality + 1
+                                item.incrementQuality()
                         if item.sell_in < 6:
                             if isUnderHighestQualityValue(item):
-                                item.quality = item.quality + 1
+                                item.incrementQuality()
             if not itemIsNamed(item, "Sulfuras, Hand of Ragnaros"):
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
                 if not itemIsNamed(item, "Aged Brie"):
                     if not itemIsNamed(item, "Backstage passes to a TAFKAL80ETC concert"):
-                        if item.quality > 0:
+                        if itemHasNonZeroValue(item):
                             if not itemIsNamed(item, "Sulfuras, Hand of Ragnaros"):
-                                item.quality = item.quality - 1
+                                item.decreaseQualityBy(1)
                     else:
-                        item.quality = item.quality - item.quality
+                        item.decreaseQualityBy(item.quality)
                 else:
                     if item.quality < 50:
-                        item.quality = item.quality + 1
+                        item.incrementQuality()
 
 
 class Item:
@@ -50,3 +48,15 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+    def incrementQuality(self):
+        self.quality += 1
+
+    def decreaseQualityBy(self, decrease):
+        self.quality -= decrease
+
+    def isItemConjured(self):
+        return "Conjured" in self.name
+
+    def isItemSulfuras(self):
+        return "Sulfuras" in self.name
